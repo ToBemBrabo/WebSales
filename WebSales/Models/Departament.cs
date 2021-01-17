1cfd925581d0f01e9e90d1c1fc07242b706e540d
ToBemBrabo <jade820ch@outlook.com>
2021-01-17 18:55:19 -0300

    Add project files.

---
+++ WebSales/Models/Departament.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSales.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+
+        public Department()
+        {       
+        }
+    }
+}
---
+++ WebSales/Models/Enuns/SaleStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSales.Models.Enuns
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0,
+        Build = 1,
+        Canceled = 2
+    }
+}
